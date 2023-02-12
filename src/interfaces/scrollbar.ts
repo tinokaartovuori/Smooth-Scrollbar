78b76ab8394aa7c87db08137da0118f42111e151
tinokaartovuori <tino.kaartovuori@gmail.com>
2023-02-12 14:59:23 +0200

    fix: type ScrollbarOptions

--- src/interfaces/scrollbar.ts
+++ src/interfaces/scrollbar.ts
@@ -1,8 +1,6 @@
-import {
-  TrackController,
-} from './track';
+import { TrackController } from "./track";
 
-import { Data2d } from './data-2d';
+import { Data2d } from "./data-2d";
 
 // Scrollbar.options
 export type ScrollbarOptions = {
@@ -10,65 +8,70 @@ export type ScrollbarOptions = {
    * Momentum reduction damping factor, a float value between `(0, 1)`. The lower the value is, the more smooth the scrolling will be (also the more paint frames).
    * @default 0.1
    */
-  damping: number,
+  damping: number;
   /**
    *  Minimal size for scrollbar thumbs.
    * @default 20
    */
-  thumbMinSize: number,
+  thumbMinSize: number;
   /**
    * Render every frame in integer pixel values, set to `true` to **improve** scrolling performance.
    * @default true
    */
-  renderByPixels: boolean,
+  renderByPixels: boolean;
+  /**
+   * Set to `true` to use an external RAF loop. This requires wiring up/calling the scrollbar render() function yourself.
+   * @default false
+   */
+  externalRAF: boolean;
   /**
    * Keep scrollbar tracks visible.
    * @default false
    */
-  alwaysShowTracks: boolean,
+  alwaysShowTracks: boolean;
   /**
    * Set to `true` to allow outer scrollbars continue scrolling when current scrollbar reaches edge.
    * @default true
    */
-  continuousScrolling: boolean,
+  continuousScrolling: boolean;
   /**
    *  Delegate wheel events and touch events to the given element. By default, the container element is used. This option will be useful for dealing with fixed elements.
    * @default null
    */
-  delegateTo: EventTarget | null,
+  delegateTo: EventTarget | null;
   /**
    * @deprecated `wheelEventTarget` is deprecated and will be removed in the future, use `delegateTo` instead.
    */
-  wheelEventTarget: EventTarget | null,
+  wheelEventTarget: EventTarget | null;
   /**
    * Options for plugins, see {@link https://github.com/idiotWu/smooth-scrollbar/blob/develop/docs/plugin.md Plugin System}.
    */
-  plugins: any,
+  plugins: any;
 };
 
 // Scrollbar.size
 export type Metrics = {
-  width: number,
-  height: number,
+  width: number;
+  height: number;
 };
 
 export type ScrollbarSize = {
-  container: Metrics,
-  content: Metrics,
+  container: Metrics;
+  content: Metrics;
 };
 
 // Scrollbar.bounding
 export type ScrollbarBounding = {
-  top: number,
-  right: number,
-  bottom: number,
-  left: number,
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
 };
 
 // Scrolling Listener
 export type ScrollStatus = {
-  offset: Data2d,
-  limit: Data2d,
+  offset: Data2d;
+  limit: Data2d;
 };
 
 export interface ScrollListener {
@@ -77,22 +80,22 @@ export interface ScrollListener {
 
 // `scrollTo` options
 export type ScrollToOptions = {
-  callback: (this: Scrollbar) => void,
-  easing: (percent: number) => number,
+  callback: (this: Scrollbar) => void;
+  easing: (percent: number) => number;
 };
 
 // `setPosition` options
 export type SetPositionOptions = {
-  withoutCallbacks: boolean,
+  withoutCallbacks: boolean;
 };
 
 // `scrollIntoView` options
 export type ScrollIntoViewOptions = {
-  alignToTop: boolean,
-  onlyScrollIfNeeded: boolean,
-  offsetTop: number,
-  offsetLeft: number,
-  offsetBottom: number,
+  alignToTop: boolean;
+  onlyScrollIfNeeded: boolean;
+  offsetTop: number;
+  offsetLeft: number;
+  offsetBottom: number;
 };
 
 export interface AddTransformableMomentumCallback {
@@ -130,13 +133,30 @@ export interface Scrollbar {
   addListener(fn: ScrollListener): void;
   removeListener(fn: ScrollListener): void;
 
-  addTransformableMomentum(x: number, y: number, fromEvent: Event, callback?: AddTransformableMomentumCallback): void;
+  addTransformableMomentum(
+    x: number,
+    y: number,
+    fromEvent: Event,
+    callback?: AddTransformableMomentumCallback
+  ): void;
   addMomentum(x: number, y: number): void;
   setMomentum(x: number, y: number): void;
 
-  scrollTo(x?: number, y?: number, duration?: number, options?: Partial<ScrollToOptions>): void;
-  setPosition(x?: number, y?: number, options?: Partial<SetPositionOptions>): void;
-  scrollIntoView(elem: HTMLElement, options?: Partial<ScrollIntoViewOptions>): void;
+  scrollTo(
+    x?: number,
+    y?: number,
+    duration?: number,
+    options?: Partial<ScrollToOptions>
+  ): void;
+  setPosition(
+    x?: number,
+    y?: number,
+    options?: Partial<SetPositionOptions>
+  ): void;
+  scrollIntoView(
+    elem: HTMLElement,
+    options?: Partial<ScrollIntoViewOptions>
+  ): void;
 
   updatePluginOptions(pluginName: string, options?: any): void;
 }
